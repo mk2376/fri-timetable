3712ce9ac1e441c38100681da601bc7d2ff7f745
mk2376 <96266061+mk2376@users.noreply.github.com>
2024-12-17 23:41:59 +0100

    Added support for Docker

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import pkg from "./package.json";
-import { qwikReact } from "@builder.io/qwik-react/vite";
 type PkgDep = Record<string, string>;
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
   dependencies: PkgDep;
@@ -25,7 +24,6 @@ export default defineConfig(({ command, mode }): UserConfig => {
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
-      qwikReact(),
     ],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
